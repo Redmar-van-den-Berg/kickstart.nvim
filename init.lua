91f54af16d3a86b0c7334019bc065890c9b08cc8
trijntje <redmar@ubuntu.com>
2023-04-18 11:24:54 +0200

    Set folding

--- init.lua
+++ init.lua
@@ -497,3 +497,7 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+-- Folding settings
+vim.opt.foldlevel = 10
+-- vim.opt.foldmethod = "indent"
